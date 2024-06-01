06e8524dc421a6147755f68c4490dd2f7912d89c
tejsingh <tejsingh7jan@gmail.com>
2024-06-01 09:16:15 +0530

    CICD Pipeline Demo Project

---
+++ src/main/java/com/tcs/trs/JenkinsCicdSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.trs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCicdSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tcs/trs/controller/CICDController.java
@@ -0,0 +1,14 @@
+package com.tcs.trs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CICDController {
+
+	@GetMapping("/test")
+	public String showMeassge()
+	{
+		return "Welcome to CICD Pipeline Demo Project";
+	}
+}
